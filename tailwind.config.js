b27692af2e450470ab1973c2653df3a2733fb535
vince-dev-it <janvincentn.toledo@gmail.com>
2024-11-22 11:47:17 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.jsx",
+        "./resources/**/*.js",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
